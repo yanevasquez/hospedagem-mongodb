49b6345a3bc9376bb3a54ad19251c1d4cb23bbf6
adriandersonlira <adriandersonmusica@gmail.com>
2021-06-03 02:45:43 -0300

    Update updates_remove.js

--- scripts/updates_remove.js
+++ scripts/updates_remove.js
@@ -2,8 +2,10 @@
 
 // 02 atualizações
 
+db.acomodacao.updateOne( { "nome" : "Casa na praia" }, { $set: { "diaria" : 160 } })
 
-
+db.acomodacao.updateMany( { "diaria": { $gte: 100 } }, { $set: { "status" : "D" } } )
 
 // 01 remoção 
 
+db.reserva.deleteMany({ "acomodacao": ObjectId("60aed62f0213426e9eff3a6e") })
